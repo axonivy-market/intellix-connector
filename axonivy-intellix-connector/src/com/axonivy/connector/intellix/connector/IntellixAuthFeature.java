383d418b23a132c275de674be7af2a51aa32297a
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2022-06-14 16:19:01 +0200

    send invoice using cookie aware authfeature
    - premature feature: forked from docuware connector

---
+++ axonivy-intellix-connector/src/com/axonivy/connector/intellix/connector/IntellixAuthFeature.java
@@ -0,0 +1,229 @@
+package com.axonivy.connector.intellix.connector;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.ws.rs.Priorities;
+import javax.ws.rs.client.ClientRequestContext;
+import javax.ws.rs.client.ClientRequestFilter;
+import javax.ws.rs.client.ClientResponseContext;
+import javax.ws.rs.client.ClientResponseFilter;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Configuration;
+import javax.ws.rs.core.Feature;
+import javax.ws.rs.core.FeatureContext;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
+import javax.ws.rs.core.Response;
+
+import org.apache.commons.lang3.StringUtils;
+
+import ch.ivyteam.ivy.data.cache.IDataCacheEntry;
+import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.rest.client.FeatureConfig;
+import ch.ivyteam.ivy.rest.client.internal.ExternalRestWebServiceCall;
+
+/**
+ * Feature to make sure, that we have the DocuWare cookies.
+ *
+ * If they are not there, make a login request.
+ */
+@SuppressWarnings("restriction")
+public class IntellixAuthFeature implements Feature, ClientRequestFilter, ClientResponseFilter {
+
+	private static final String ACCOUNT_LOGON_PATH = "Account/Login";
+	private static final String USERNAME_PROPERTY = "UserName";
+	private static final String PASSWORD_PROPERTY = "Password";
+	private static final String LOGONURL_PROPERTY = "LogonUrl";
+
+	@Override
+	public boolean configure(FeatureContext context) {
+		Ivy.log().debug("DocuWare feature configure.");
+		invalidateCookiesCacheEntry();
+		context.register(this, Priorities.AUTHORIZATION);
+		return true;
+	}
+
+	@Override
+	public void filter(ClientRequestContext reqContext) throws IOException {
+		if(isLogonRequest(reqContext.getUri())) {
+			Ivy.log().info("DocuWare logon request.");
+		}
+		else {
+			if(getCookies() == null) {
+				Ivy.log().info("DocuWare logon, fetching cookies.");
+
+				Configuration configuration = reqContext.getConfiguration();
+
+				FeatureConfig config = new FeatureConfig(configuration, IntellixAuthFeature.class);
+
+				Form form = new Form()
+						.param("UserName", config.readMandatory(USERNAME_PROPERTY))
+						.param("Password", config.readMandatory(PASSWORD_PROPERTY))
+						;
+
+				String logonUrl = config.read(LOGONURL_PROPERTY).orElse(null);
+
+				if(StringUtils.isBlank(logonUrl) || logonUrl.trim().equalsIgnoreCase("AUTO")) {
+					try {
+						// Note: this API is not public and will probably change in future Ivy versions.
+						ExternalRestWebServiceCall externalRestWebServiceCall = (ExternalRestWebServiceCall) reqContext.getProperty(ExternalRestWebServiceCall.class.getCanonicalName());
+						if(externalRestWebServiceCall != null) {
+							// String host = Ivy.var().get("docuware-connector.host");
+							String host = reqContext.getUri().getHost();
+
+							logonUrl = externalRestWebServiceCall.getWebTarget().resolveTemplate("host", host).path(ACCOUNT_LOGON_PATH).getUri().toString();
+						}
+					} catch (Throwable t) {
+						String message = String.format("Could not determine DocuWare target URL automatically, please set it in REST client property '%s'. Put there the same URL as used for the client.", LOGONURL_PROPERTY);
+						Ivy.log().error(message, t);
+						reqContext.abortWith(Response.status(Response.Status.PRECONDITION_FAILED)
+								.type(MediaType.TEXT_PLAIN)
+								.entity(message)
+								.build());
+					}
+				}
+
+				Ivy.log().info("DocuWare logon via URL {0}", logonUrl);
+
+				Response response = logon(reqContext, form, logonUrl);
+
+				Ivy.log().info("DocuWare logon returned with status {0}", response.getStatusInfo());
+
+				if(getCookies() == null) {
+					Ivy.log().error("DocuWare logon, still missing DocuWare cookies for context {0} after logon.", reqContext);
+				}
+			}
+
+			requestHook(reqContext);
+		}
+	}
+
+	protected Response logon(ClientRequestContext reqContext, Form form, String target) {
+		Response response = reqContext.getClient()
+				.target(target)
+				.request(MediaType.APPLICATION_FORM_URLENCODED_TYPE)
+				.accept(MediaType.APPLICATION_JSON)
+				.post(Entity.form(form));
+		return response;
+	}
+
+	/**
+	 * Manipulate request if needed (probably only useful for tests).
+	 *
+	 * @param reqContext
+	 */
+	protected void requestHook(ClientRequestContext reqContext) {
+	}
+
+	@Override
+	public void filter(ClientRequestContext reqContext, ClientResponseContext rspContext) throws IOException {
+		if(isLogonRequest(reqContext.getUri())) {
+			int status = rspContext.getStatus();
+			if(status >= 200 && status < 300) {
+				Ivy.log().info("DocuWare logon successful: Status: {1}", status);
+
+				DocuWareCookies docuWareCookies = DocuWareCookies.create(rspContext.getCookies());
+				if(true) {
+					setCookiesCacheEntry(docuWareCookies);
+				}
+			}
+			else {
+				Ivy.log().error("DocuWare logon unsuccessful, URI: {0} Status: {1}", reqContext.getUri(), status);
+			}
+		}
+	}
+
+	/**
+	 * Get the DocuWare cookies from the session.
+	 */
+	public DocuWareCookies getCookies() {
+		DocuWareCookies docuWareCookies = null;
+		try {
+			IDataCacheEntry entry = getCookiesCacheEntry();
+			if(entry == null || !entry.isValid()) {
+				Ivy.log().info("Got expired DocuWare cookies.");
+			}
+			else {
+				docuWareCookies = (DocuWareCookies) entry.getValue();
+			}
+		} catch (Exception e) {
+			Ivy.log().error("Exception while getting cached DocuWare cookies.", e);
+		}
+		return docuWareCookies;
+	}
+
+	/**
+	 * Get the cache entry of the DocuWare cookies.
+	 */
+	public IDataCacheEntry getCookiesCacheEntry() {
+		return Ivy.datacache().getAppCache().getEntry(DocuWareCookies.DW_COOKIES_CACHE_GROUP_ID, DocuWareCookies.DW_COOKIES_CACHE_ID);
+	}
+
+	/**
+	 * Cache the DocuWare cookies.
+	 *
+	 * @param docuWareCookies
+	 */
+	public void setCookiesCacheEntry(DocuWareCookies docuWareCookies) {
+		Ivy.log().info("Caching new DocuWare cookies: {0}", docuWareCookies);
+		Ivy.datacache().getAppCache().setEntry(DocuWareCookies.DW_COOKIES_CACHE_GROUP_ID, DocuWareCookies.DW_COOKIES_CACHE_ID, DocuWareCookies.DW_COOKIES_EXPIRY_SECONDS, docuWareCookies);
+	}
+
+	/**
+	 * Invalidate the cached DocuWare cookies.
+	 */
+	public void invalidateCookiesCacheEntry() {
+		IDataCacheEntry entry = getCookiesCacheEntry();
+		if(entry != null) {
+			Ivy.log().info("Invalidating DocuWare cookies: {0}", entry.getValue());
+			entry.invalidate();
+		}
+	}
+
+	/**
+	 * Is this a logon request?
+	 * @param uri
+	 */
+	private boolean isLogonRequest(URI uri) {
+		return uri != null && uri.toString().endsWith(ACCOUNT_LOGON_PATH);
+	}
+
+	/**
+	 * Container for the cookies from DocuWare.
+	 *
+	 * The DocuWare Cookies have a very long life-time (some days), but here
+	 * we go down to one day. It will be longer than the Ivy session.
+	 */
+	public static class DocuWareCookies {
+		public static final String DW_COOKIES_CACHE_ID = "cookies";
+		public static final String DW_COOKIES_CACHE_GROUP_ID = "com.docuware.cookies";
+		public static final int DW_COOKIES_EXPIRY_SECONDS = 86400;
+                private final Map<String, NewCookie> cookies;
+
+
+		private DocuWareCookies(Map<String, NewCookie> cookies) {
+			if(Ivy.log().isDebugEnabled()) {
+				for (Entry<String, NewCookie> entry : cookies.entrySet()) {
+					NewCookie cookie = entry.getValue();
+					Ivy.log().debug("Received cookie: {0}: Name: {1} Expiry: {2} Max Age: {3}", entry.getKey(), cookie.getName(), cookie.getExpiry(), cookie.getMaxAge());
+				}
+			}
+			this.cookies = cookies;
+		}
+
+		/**
+		 * Create a new {@link DocuWareCookies} from the cookies received in the REST client response.
+		 *
+		 * @param cookies
+		 */
+		public static DocuWareCookies create(Map<String, NewCookie> cookies) {
+			DocuWareCookies docuWareCookies = new DocuWareCookies(cookies);
+			return docuWareCookies;
+		}
+
+	}
+}
--- axonivy-intellix-demo/processes/intellixDemo.mod
+++ axonivy-intellix-demo/processes/intellixDemo.mod
@@ -12,34 +12,74 @@ io0 @TextInP .responsibility .responsibility #zField
 io0 @StartRequest f0 '' #zField
 io0 @EndTask f1 '' #zField
 io0 @RestClientCall f3 '' #zField
-io0 @PushWFArc f4 '' #zField
 io0 @PushWFArc f2 '' #zField
+io0 @GridStep f6 '' #zField
+io0 @PushWFArc f7 '' #zField
+io0 @PushWFArc f4 '' #zField
 >Proto io0 io0 intellixDemo #zField
-io0 f0 outLink start.ivp #txt
+io0 f0 outLink uploadSampleInvoice.ivp #txt
 io0 f0 inParamDecl '<> param;' #txt
 io0 f0 requestEnabled true #txt
 io0 f0 triggerEnabled false #txt
-io0 f0 callSignature start() #txt
+io0 f0 callSignature uploadSampleInvoice() #txt
 io0 f0 caseData businessCase.attach=true #txt
 io0 f0 @CG|tags demo #txt
 io0 f0 @C|.xml '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <elementInfo>
     <language>
-        <name>start.ivp</name>
+        <name>uploadSampleInvoice.ivp</name>
     </language>
 </elementInfo>
 ' #txt
 io0 f0 @C|.responsibility Everybody #txt
 io0 f0 81 49 30 30 -25 17 #rect
-io0 f1 337 49 30 30 0 15 #rect
-io0 f3 168 42 112 44 0 -7 #rect
-io0 f4 111 64 168 64 #arcP
-io0 f2 280 64 337 64 #arcP
+io0 f1 497 49 30 30 0 15 #rect
+io0 f3 clientId a3fb2ba0-71ab-49a6-82fe-bb0c202b38bd #txt
+io0 f3 path documents #txt
+io0 f3 queryParams 'extract=true;
+' #txt
+io0 f3 templateParams 'host=;
+' #txt
+io0 f3 headers 'Accept=application/json;
+' #txt
+io0 f3 method POST #txt
+io0 f3 bodyInputType FORM #txt
+io0 f3 bodyMediaType multipart/form-data #txt
+io0 f3 bodyForm 'file=in.file;
+msName="Default_axonivy";
+' #txt
+io0 f3 @C|.xml '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<elementInfo>
+    <language>
+        <name>send invoice
+</name>
+    </language>
+</elementInfo>
+' #txt
+io0 f3 328 42 112 44 -39 -15 #rect
+io0 f2 440 64 497 64 #arcP
+io0 f6 actionTable 'out=in;
+' #txt
+io0 f6 actionCode 'import com.axonivy.connector.intellix.FileLoaderUtil;
+out.file = FileLoaderUtil.loadFromWebContent("sample/rechnung.pdf");' #txt
+io0 f6 @C|.xml '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<elementInfo>
+    <language>
+        <name>load sample
+invoice</name>
+    </language>
+</elementInfo>
+' #txt
+io0 f6 168 42 112 44 -38 -15 #rect
+io0 f7 111 64 168 64 #arcP
+io0 f4 280 64 328 64 #arcP
 >Proto io0 .type com.axonivy.connector.intellix.demo.Data #txt
 >Proto io0 .processKind NORMAL #txt
 >Proto io0 0 0 32 24 18 0 #rect
 >Proto io0 @|BIcon #fIcon
-io0 f0 mainOut f4 tail #connect
-io0 f4 head f3 mainIn #connect
 io0 f3 mainOut f2 tail #connect
 io0 f2 head f1 mainIn #connect
+io0 f0 mainOut f7 tail #connect
+io0 f7 head f6 mainIn #connect
+io0 f6 mainOut f4 tail #connect
+io0 f4 head f3 mainIn #connect
