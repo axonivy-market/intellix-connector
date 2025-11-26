c96c61cc0afe98795cc8dec8b9fe8bd1f7eafb7f
Pham Hoang Hung <84316773+phhung-axonivy@users.noreply.github.com>
2025-11-26 14:47:47 +0700

    MARP-3319 Load File from CMS (#59)

--- axonivy-intellix-demo/src/com/axonivy/connector/intellix/FileLoaderUtil.java
+++ axonivy-intellix-demo/src/com/axonivy/connector/intellix/FileLoaderUtil.java
@@ -2,25 +2,19 @@ package com.axonivy.connector.intellix;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.CoreException;
-
-import ch.ivyteam.ivy.IvyConstants;
 import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.scripting.objects.File;
 
-@SuppressWarnings("restriction")
 public class FileLoaderUtil {
 
-  public static ch.ivyteam.ivy.scripting.objects.File loadFromWebContent(String relativePath) throws IOException, CoreException {
-    IProject project = Ivy.request().getProcessModelVersion().getProject();
-    IFile file = project.getFolder(IvyConstants.DIRECTORY_WEB_CONTENT).getFile(relativePath);
-    var ivyFile = new ch.ivyteam.ivy.scripting.objects.File(file.getName(), true);
-    java.nio.file.Path path = ivyFile.getJavaFile().toPath();
-    Files.copy(file.getContents(), path, StandardCopyOption.REPLACE_EXISTING);
+  public static File loadFileFromCms(String fileName, String cmsPath) throws IOException {
+    var ivyFile = new File(fileName, true);
+    Path path = ivyFile.getJavaFile().toPath();
+    Files.copy(Ivy.cms().get(cmsPath).get().value().get().read().inputStream(), path,
+        StandardCopyOption.REPLACE_EXISTING);
     return ivyFile;
   }
-
 }
