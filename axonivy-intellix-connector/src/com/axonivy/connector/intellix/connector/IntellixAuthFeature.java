064fb9868a97b3aada1b269daca45a07aaa79f9e
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2026-05-21 19:03:05 +0700

    Update IntellixAuthFeature.java

--- axonivy-intellix-connector/src/com/axonivy/connector/intellix/connector/IntellixAuthFeature.java
+++ axonivy-intellix-connector/src/com/axonivy/connector/intellix/connector/IntellixAuthFeature.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.UUID;
 
 import javax.ws.rs.Priorities;
 import javax.ws.rs.client.ClientRequestContext;
