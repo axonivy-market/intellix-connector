71b71ea8ede9c39d7023d1a67baa98b14b60f9a9
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2026-05-21 18:40:19 +0700

    Update IntellixAuthFeature.java

--- axonivy-intellix-connector/src/com/axonivy/connector/intellix/connector/IntellixAuthFeature.java
+++ axonivy-intellix-connector/src/com/axonivy/connector/intellix/connector/IntellixAuthFeature.java
@@ -38,7 +38,7 @@ public class IntellixAuthFeature implements Feature, ClientRequestFilter, Client
 	private static final String USERNAME_PROPERTY = "UserName";
 	private static final String PASSWORD_PROPERTY = "Password";
 	private static final String LOGONURL_PROPERTY = "LogonUrl";
-	private static final String PROPERTY_CLIENT_NAME = "clientName";
+	private static final String INTELLIX_CLIENT_NAME = "intellix";
 
 	@Override
 	public boolean configure(FeatureContext context) {
@@ -72,8 +72,7 @@ public class IntellixAuthFeature implements Feature, ClientRequestFilter, Client
 				if(StringUtils.isBlank(logonUrl) || logonUrl.trim().equalsIgnoreCase("AUTO")) {
 					try {
 						String host = reqContext.getUri().getHost();
-						String clientName = config.readMandatory(PROPERTY_CLIENT_NAME);
-						logonUrl = Ivy.rest().client(clientName).resolveTemplate("host", host).path(ACCOUNT_LOGON_PATH).getUri().toString();
+						logonUrl = Ivy.rest().client(INTELLIX_CLIENT_NAME).resolveTemplate("host", host).path(ACCOUNT_LOGON_PATH).getUri().toString();
 					} catch (Throwable t) {
 						String message = String.format("Could not determine DocuWare target URL automatically, please set it in REST client property '%s'. Put there the same URL as used for the client.", LOGONURL_PROPERTY);
 						throw new IllegalStateException(message, t);
